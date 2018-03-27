dcaa5454122e03f7dc93156197968155ba1dfa4f
Tuomas Katajisto <t@ktj.st>
2018-03-27 15:34:30 +0300

    Made more features to the language.

--- brainfuck.cpp
+++ brainfuck.cpp
@@ -1,18 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+int board[101010];
 int main()
 {
     bool skipForward = false;
     bool skipBackward = false;
-    int board[101010];
     for(int i = 0; i < 101010; i++) board[i] = 0;
     int pointer = 0;
     cout << "KATAJISTO'S BRAINFUCK INTERPRETER" << "\n";
-    cout << "Version 1.0.0" << "\n";
+    cout << "Version 1.0.1" << "\n";
     while(1)
     {
-
-        //cout << "POINTER IS AT: " << pointer << "\n" << ">>";
         cout << "\n~";string s; getline(cin, s);
         for(int i = 0; i < s.length(); i++)
         {
@@ -82,6 +80,21 @@ int main()
                 int asciiCode = inputChar;
                 board[pointer] = asciiCode;
             }
+            else if(cmd == ';')
+            {
+                int inputInt; cin >> inputInt;
+                board[pointer] = inputInt;
+            }
+            else if(cmd == ':')
+            {
+                cout << board[pointer];
+            }
+            else if(cmd == '@')
+            {
+                int amountToPrint; cin >> amountToPrint;
+                for(int i = 0; i < amountToPrint; i++) cout << board[i];
+            }
+            else if(cmd =='%') cout << "\n";
         }
     }
 }
